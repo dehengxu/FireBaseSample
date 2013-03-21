154f4585cfd669bb56f7b4c3da37a7aacef555a7
DehengXu <xudeheng_nx@yahoo.com.cn>
2013-03-21 17:36:47 +0800

    complete simple testing function on firebase.

---
+++ FireBaseSample/FBUtils.h
@@ -0,0 +1,13 @@
+//
+//  FBUtils.h
+//  FireBaseSample
+//
+//  Created by Deheng.Xu on 13-3-21.
+//  Copyright (c) 2013年 Nicholas.Xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBUtils : NSObject
+
+@end
---
+++ FireBaseSample/FBUtils.m
@@ -0,0 +1,13 @@
+//
+//  FBUtils.m
+//  FireBaseSample
+//
+//  Created by Deheng.Xu on 13-3-21.
+//  Copyright (c) 2013年 Nicholas.Xu. All rights reserved.
+//
+
+#import "FBUtils.h"
+
+@implementation FBUtils
+
+@end
