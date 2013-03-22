fd23a2eefd94b1c22767905cd5152f68b9c46778
DehengXu <xudeheng_nx@yahoo.com.cn>
2013-03-22 11:43:36 +0800

    add firebasedb wrapper.

---
+++ FireBaseSample/FirebaseDB.h
@@ -0,0 +1,13 @@
+//
+//  FirebaseDB.h
+//  FireBaseSample
+//
+//  Created by Deheng.Xu on 13-3-22.
+//  Copyright (c) 2013年 Nicholas.Xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirebaseDB : NSObject
+
+@end
---
+++ FireBaseSample/FirebaseDB.m
@@ -0,0 +1,13 @@
+//
+//  FirebaseDB.m
+//  FireBaseSample
+//
+//  Created by Deheng.Xu on 13-3-22.
+//  Copyright (c) 2013年 Nicholas.Xu. All rights reserved.
+//
+
+#import "FirebaseDB.h"
+
+@implementation FirebaseDB
+
+@end
